e83926b5cf2ad2a364f1db2c629faaa6fc10cf2b
Vihaan875 <76944903+Vihaan875@users.noreply.github.com>
2021-11-03 15:41:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+leftWristx=0;
+leftWristy=0;
+rightWristy=0;
+rightWristx=0;
+song1="";
+song2="";
+
 function setup()
 {
     canvas=createCanvas(600,500);
@@ -5,4 +12,22 @@ function setup()
 
     video=createCapture(VIDEO);
     video.hide();
+}
+
+function preload()
+{
+    song1=loadSound("music.mp3");
+    song2=loadSound("music2.mp3");
+}
+
+function play()
+{
+    song.play();
+}
+
+function draw()
+{
+    image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("#FF0000");
 }
